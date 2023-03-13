57af2e953187b9502acdf2997308e28f54447e44
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-03-12 23:36:06 -0700

    generate password function started with for loop

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,6 @@
+var lengthOfPassword = 0;
+var chosenArray = [];
+
 // Arrays of characters to be included in password
 var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
@@ -6,24 +9,29 @@ var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')
 
 
 // (1)Assignment Code
-var generateBtn = document.querySelector("#generate");//(BEGINprogram) BIG RED generate botton on page JS linked to HTML aka BEGINprogram
+var generateBtn = document.querySelector('#generate');//(BEGINprogram) BIG RED generate botton on page JS linked to HTML aka BEGINprogram
 
 // (2) Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
 
 // (3) Write password to the #password input
 function writePassword() {
   var password = generatePassword();//(4)undefined function. will need to create.
-  var passwordText = document.querySelector("#password"); //will send back to HTML (ENDProgram)
+  var passwordText = document.querySelector('#password'); //will send back to HTML (ENDProgram)
 
   passwordText.value = password;//--> (ENDProgram)takes value of generatePassword function and pushes it to HTML cardbody textarea aka 'ENDProgram'
 
 }
 
+
+
 // (4)
 function generatePassword(){
+  for(var i = 0; i < lengthOfPassword; i++){
+      var randomI = Math.floor(Math.random() * chosenArray.length)
+  }
 
-  return('hello');//--->(end)logs into page card. result will go here.
+  return('test');//--->(end)logs into page card. result will go here.
 }
 
 
